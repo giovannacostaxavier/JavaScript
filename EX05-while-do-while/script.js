35c8bc5fcc66707dbe19a1d28e98fed57e9551cc
Giovanna Xavier <giovannacostaxavier@gmail.com>
2025-10-30 20:50:20 +0000

    exercicio de while

---
+++ EX05-while-do-while/script.js
@@ -0,0 +1,18 @@
+//WHILE PARA CONTAR DE 1 A 50
+
+let n = 1
+while (n <= 50) {
+    //console.log(n);
+    n++  
+}
+
+//WHILE PARA MOSTRAR APENAS OS NÚMEROS PARES DE 1 A 100
+
+let numero = 0
+while (numero <= 100) {
+    if (numero % 2 == 0) {
+        console.log(numero);
+    }
+    numero ++
+}
+
