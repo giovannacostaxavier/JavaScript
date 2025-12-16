ba88c5eb9d4707871c3e9aaf0fde8e540f8cc02f
Giovanna Xavier <giovannacostaxavier@gmail.com>
2025-12-16 13:10:51 +0000

    Exercicios de map usando o DOM

---
+++ EX07-TRABALHO-MAP/script.js
@@ -0,0 +1,14 @@
+//const TodasDivs=[...document.querySelectorAll(".divs")]
+   // TodasDivs.map((el)=>{
+   //     el.addEventListener("click", () => {
+   //     el.classList.add("destaque");
+   // })
+
+   // })
+
+   const c1=document.getElementById("c1")
+    c1.addEventListener("click",()=>{
+        alert("Clicou!")
+    })
+  
+
