1c285428ee19154f04f06515dad21807313eeac9
Giovanna Xavier <giovannacostaxavier@gmail.com>
2025-12-30 12:00:43 +0000

    atualizaçoes no exercicio

--- EX07-TRABALHO-MAP/script.js
+++ EX07-TRABALHO-MAP/script.js
@@ -2,13 +2,25 @@ const caixa1=document.querySelector("#caixa1")
 const caixa2=document.querySelector("#caixa2")
 const btn=document.querySelector(".btn")
 const cursos=[...document.querySelectorAll(".cursos")]
+    
+const novaDiv=document.createElement("div")
+novaDiv.setAttribute("class","cursos")
+novaDiv.setAttribute("id","c10")
+novaDiv.innerHTML="Canvas"
+caixa1.appendChild(novaDiv)
 
-cursos.forEach((el)=>{
-    el.addEventListener("click",(evt)=>{
-        const todosCursos= evt.target
-        todosCursos.classList.toggle("selecionado")
-    })
-})
+// cursos.forEach((el)=>{
+//     el.addEventListener("click",(evt)=>{
+//         const todosCursos= evt.target
+//         todosCursos.classList.toggle("selecionado")
+//     })
+// })
+caixa1.addEventListener("click", (evt) => {
+    if (evt.target.classList.contains("cursos")) {
+        evt.target.classList.toggle("selecionado");
+    }
+});
+
 
 btn.addEventListener("click",()=>{
     const cursoSelecionado=[...document.querySelectorAll(".selecionado")]
