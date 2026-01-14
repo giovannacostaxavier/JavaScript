e813cc1001973eb7973e0f7a8c72c6e91dc23219
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-01-14 20:13:35 +0000

    Atualizações do dia 14/01/26

--- EX08-removendo-e-add-itens/script.js
+++ EX08-removendo-e-add-itens/script.js
@@ -1,7 +1,29 @@
 const cursoSel = document.getElementById("cursoSelecionado");
+const removerbtn = document.getElementById("btnRemover");
 
+
+const funcaoBTN =()=>{
+return document.querySelector('input[type="radio"]:checked');
+}
 cursoSel.addEventListener("click", () => {
-    const radioSelecionado = document.querySelector('input[type="radio"]:checked');
-    const texto = radioSelecionado.previousElementSibling.textContent;
+    const radioSelecionado=funcaoBTN()
+    //if(!radioSelecionado){
+    //    alert("Não existem cursos selecionados, por favor selecione um curso")
+    //    return
+    //}  
+    try{const texto = radioSelecionado.previousElementSibling.textContent;
     alert(texto);
+    }catch (erro){
+        alert("Não existem cursos selecionados, por favor selecione um curso",erro)
+    }
+    
 });
+
+removerbtn.addEventListener("click",()=>{
+    const btnRemovido=funcaoBTN()
+    if(!btnRemovido){
+        alert("Não existem cursos selecionados, por favor selecione um curso")
+        return
+    }
+    btnRemovido.parentNode.remove()
+})    
