6e90cb95c0899ef40fc06aa9fcfbaf6d2d961c0c
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-02-09 15:30:58 +0000

    finalizando a calculadora

--- EX09-Calculadora/script.js
+++ EX09-Calculadora/script.js
@@ -2,36 +2,34 @@ let btnSoma=document.querySelector("#btnSoma")
 let btnSubtracao=document.querySelector("#btnSubtracao")
 let btnMultiplicacao=document.querySelector("#btnMultiplicacao")
 let btnDivisao=document.querySelector("#btnDivisao")
+let resultado=document.querySelector("#resultado")
+
 const op = [
-    (valores)=>{
-        let res = 0
-        for (v of valores) {
-            res += v 
-        }
-        return res
+    ()=>{
+        let valor1=document.querySelector("#t1").value
+        let valor2=document.querySelector("#t2").value
+        resultado.value=Number(valor1)+Number(valor2)
     },
 
-    (valores)=>{
-        let res = 0
-        for (v of valores) {
-            res -= v 
-        }
-        return res
+    ()=>{
+        let valor1=document.querySelector("#t1").value
+        let valor2=document.querySelector("#t2").value
+        resultado.value=Number(valor1)-Number(valor2)
     },
 
-    (valores)=>{
-        let res = 1
-        for (v of valores) {
-            res *= v 
-        }
-        return res
+    ()=>{
+        let valor1=document.querySelector("#t1").value
+        let valor2=document.querySelector("#t2").value
+        resultado.value=Number(valor1)*Number(valor2)
     },
 
-    (valores)=>{
-        let res = 0
-        for (v of valores) {
-            res /= v 
-        }
-        return res
+    ()=>{
+        let valor1=document.querySelector("#t1").value
+        let valor2=document.querySelector("#t2").value
+        resultado.value=Number(valor1)/Number(valor2)
     }
 ]
+    btnSoma.addEventListener('click',op[0])
+    btnSubtracao.addEventListener('click',op[1])
+    btnMultiplicacao.addEventListener('click',op[2])
+    btnDivisao.addEventListener('click',op[3])
