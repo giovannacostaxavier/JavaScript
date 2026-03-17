d866335a1a54c6bf261e891652dc17a00b33ad92
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-03-17 17:08:09 +0000

    Exercicio de JS

---
+++ EXERCICIOS_PRATICOS/EX-24/script.js
@@ -0,0 +1,9 @@
+let frutas = ["Maça","Banana","Laranja","Melancia","Uva"]
+let listaUl = document.createElement('ul')
+let body = document.getElementsByTagName('body')
+body[0].appendChild(listaUl)
+for(i=0;i<frutas.length;i++){
+    let itens = document.createElement('li')
+    let t
+}
+
