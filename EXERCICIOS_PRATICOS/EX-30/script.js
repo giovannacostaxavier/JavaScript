94f084dcc78d369d7de1ccfa2015b3f04d961530
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-03-24 16:59:27 +0000

    novo exercicio js

---
+++ EXERCICIOS_PRATICOS/EX-30/script.js
@@ -0,0 +1,10 @@
+//FUNÇÃO QUE CALCULA MÉDIA DE UM ARRAY
+let num = [1,2,3,4,5]
+let soma = 0
+let media = 0
+let op= num.forEach((el)=>{
+    return soma += el
+})
+console.log(soma/num.length);
+
+ 
