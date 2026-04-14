39c0aa6d1b1f4178b4a1587bced799ec3aa0799b
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-04-14 17:21:11 +0100

    Exercicio 08 ficha 2

---
+++ EXERCICIOS_PRATICOS_02/EX08/script.js
@@ -0,0 +1,13 @@
+//VERIFICAR SE NÚMERO ESTÁ EM ARRAY COM INCLUDES
+
+let n = [7,12,15,80,92];
+let nomes = ["Bruna","Ana","Paula"];
+
+console.log(n.includes(12));
+
+console.log(nomes.includes("Ana"));
+
+console.log(nomes.includes("e"));
+
+
+
