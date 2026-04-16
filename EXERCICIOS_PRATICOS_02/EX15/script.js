51ed1acebd58a22bc27c5018f9c7367bcf06e8a9
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-04-16 17:05:25 +0100

    Exercicio 15 ficha 2

---
+++ EXERCICIOS_PRATICOS_02/EX15/script.js
@@ -0,0 +1,12 @@
+//FUNÇÃO QUE TRANSFORMA ARRAY DE STRINGS EM ARRAY DE COMPRIMENTOS
+
+
+let nomes = ["Koll","Finn","Giovanna","Pedro"];
+
+let cump = nomes.map((el)=>{
+    return el.length
+})
+
+console.log(cump);
+
+
