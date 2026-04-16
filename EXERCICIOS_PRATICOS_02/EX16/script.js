52aaf19595bcff3ac0efb56dc8e1386b623ccb39
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-04-16 17:17:58 +0100

    Exercicio 16 ficha 2

---
+++ EXERCICIOS_PRATICOS_02/EX16/script.js
@@ -0,0 +1,8 @@
+
+
+let procurar = (array,valor)=>{
+    return array.indexOf(valor)
+}
+
+console.log(procurar(["Ana", "Paula", "Carol"],"Paula"))
+console.log(procurar(["Ana", "Paula", "Carol"], "João"))
