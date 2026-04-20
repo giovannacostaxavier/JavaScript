24dca307266173fdcf66da142d41eb0604c3c94a
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-04-20 16:52:37 +0100

    Exercicio 52

---
+++ EXERCICIOS_PRATICOS/EX-52/script.js
@@ -0,0 +1,8 @@
+//FUNÇÃO QUE RECEBE UM NÚMERO E RETORNA 'PAR' OU 'ÍMPAR' USANDO OPERADOR TERNÁRIO
+
+let imparOuPar = (a)=>{
+    return a % 2 == 0 ? "Par" : "impar"
+    
+}
+
+console.log(imparOuPar(5));
