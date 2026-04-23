e7862462da7c555c0c65c6e84a683e4d59bd9fce
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-04-23 09:32:47 +0100

    Exercicio 56

---
+++ EXERCICIOS_PRATICOS/EX-56/script.js
@@ -0,0 +1,8 @@
+//Função que recebe um valor e usa typeof para retornar uma mensagem descrevendo o tipo "É um número"
+
+let tipo =(a)=>{
+    return `É um: ${typeof a}`
+}
+
+console.log(tipo('eu'));
+
